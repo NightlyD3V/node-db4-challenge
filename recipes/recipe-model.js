e793620f8eac6e4c6414a106e9120f22c0542f06
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-08-29 20:24:29 -0400

    Boilerplating for knexfile, migrations and recipe-model

---
+++ recipes/recipe-model.js
@@ -0,0 +1,20 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstruction
+
+};
+
+function getRecipes() {
+
+};
+
+function getShoppingList(recipe_id) {
+
+};
+
+function getInstruction(recipe_id) {
+
+};
